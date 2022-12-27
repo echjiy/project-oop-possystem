904dd5563903ed2f6570ccc924ce7abfd5bb6ce1
Iwin123 <buangmo90@gmail.com>
2022-12-27 23:33:23 +0800

    updating admin dashboard footer

--- pos/admin/dashboard.php
+++ pos/admin/dashboard.php
@@ -211,7 +211,6 @@ require_once('partials/_analytics.php');
         </div>
       </div>
       <!-- Footer -->
-      <?php require_once('partials/_footer.php'); ?>
     </div>
   </div>
   <!-- Argon Scripts -->
