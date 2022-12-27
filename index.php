b361a30fcc95548ba7d6ea1a262509718bc8ecb8
Iwin123 <buangmo90@gmail.com>
2022-12-27 22:34:21 +0800

    update admin title head

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>POS System</title>
+    <title>Point of Sale and
+Inventory Management</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
