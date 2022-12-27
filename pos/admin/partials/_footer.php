d5c5e25459736cc7d3eba566bc8771bfc6663451
Iwin123 <buangmo90@gmail.com>
2022-12-27 22:46:34 +0800

    update footer

--- pos/admin/partials/_footer.php
+++ pos/admin/partials/_footer.php
@@ -8,7 +8,7 @@
       <div class="col-xl-6">
         <ul class="nav nav-footer justify-content-center justify-content-xl-end">
           <li class="nav-item">
-            <a href="" class="nav-link" target="_blank">Restaurant POS</a>
+            <a href="" class="nav-link" target="_blank"></a>
           </li>
         </ul>
       </div>
