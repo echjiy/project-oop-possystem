069c8fc33cd59a43e956c0a16353ea8b105dcd59
Iwin123 <buangmo90@gmail.com>
2022-12-27 22:39:26 +0800

    update index.php

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Point of Sale and
-Inventory Management</title>
+    <title> Point of Sale and
+Inventory Management </title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
